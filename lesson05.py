6811bfffa90975aaf433bffad4fd4720dde1c82f
Zain-Zameer <officialmuhammadzain45@gmail.com>
2024-03-13 08:10:19 -0700

    Work added

---
+++ lesson05.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# arr = np.array([1, 2, 3, 4])
+# print(arr.dtype)
+
+# arr = np.array([1, 2, 3, 4], dtype='i4')
+# print(arr)
+# print(arr.dtype)
+
+# arr = np.array([1.1, 2.1, 3.1])
+# newarr = arr.astype('i')
+# print(newarr)
+# print(newarr.dtype)
+
+# arr = np.array([1.1, 2.1, 3.1])
+# newarr = arr.astype(str)
+# print(newarr)
+# print(newarr.dtype)
+
+
+# i - integer
+# b - boolean
+# u - unsigned integer
+# f - float
+# c - complex float
+# m - timedelta
+# M - datetime
+# O - object
+# S - string
+# U - unicode string
+# V - fixed chunk of memory for other type ( void )
---
+++ lesson07.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+rr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print(rr.shape)
+
+
+
+#Returns row & column of set
+
+# arr = np.array([1, 2, 3, 4, 5], ndmin=5)
+
+# print(arr)
+# print('shape of array :', arr.shape)
